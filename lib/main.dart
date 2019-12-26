ac426ac980bd8b2d7012ab16f17cec8ae153b175
Jie liang Huang <>
2019-12-26 23:11:54 +0800

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,85 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(Calculator());
+}
 
-class MyApp extends StatelessWidget {
-
+class Calculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Dem rqer'),
+      debugShowCheckedModeBanner: false,
+      title: 'simple calculator',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: SimpleCalculator(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class SimpleCalculator extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _SompleCalculatorState createState() => _SompleCalculatorState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _SompleCalculatorState extends State<SimpleCalculator> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      appBar: AppBar(title: Text('Simple Calculator')),
+      body: Column(
+        children: <Widget>[
+
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+            child: Text('0', style: TextStyle(fontSize: 38.0),),
+          ),
+
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+            child: Text('0', style: TextStyle(fontSize: 38.0),),
+          ),
+
+          Expanded(
+            child: Divider()
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width * .75,
+                child: Table(
+                  children: [
+                  TableRow(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.1,
+                        color: Colors.redAccent,
+                        child: FlatButton(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(0.0),
+                            side: BorderSide(
+                              color: Colors.white,
+                              width: 1,
+                              style: BorderStyle.solid
+                            )
+                          ),
+                          padding: EdgeInsets.all(16.0),
+                          onPressed: null,
+                          child: null
+                        ),
+                      )
+                    ]
+                  )
+                  ]
+                ),
+              )
+            ],
+          )
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
